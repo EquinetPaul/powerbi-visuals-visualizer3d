7f62f0fe6f30c308d8b7630aa7681d20ee21e199
PA <45755573+EquinetPaul@users.noreply.github.com>
2024-05-03 22:27:15 +0200

    initial commit

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Change Log - Visualizer3d
-This page contains information about changes to the 'Visualizer3d' Power BI visual.
-
-## 1.0.0.0
-
--- src/visual.ts
+++ src/visual.ts
@@ -1,169 +1,77 @@
+/*
+*  Power BI Visual CLI
+*
+*  Copyright (c) Microsoft Corporation
+*  All rights reserved.
+*  MIT License
+*
+*  Permission is hereby granted, free of charge, to any person obtaining a copy
+*  of this software and associated documentation files (the ""Software""), to deal
+*  in the Software without restriction, including without limitation the rights
+*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the Software is
+*  furnished to do so, subject to the following conditions:
+*
+*  The above copyright notice and this permission notice shall be included in
+*  all copies or substantial portions of the Software.
+*
+*  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+*  THE SOFTWARE.
+*/
+"use strict";
+
 import powerbi from "powerbi-visuals-api";
 import { FormattingSettingsService } from "powerbi-visuals-utils-formattingmodel";
-
-import { dataViewObjects } from "powerbi-visuals-utils-dataviewutils";
-
-import { BarChartSettingsModel } from "./settings";
-
 import "./../style/visual.less";
 
-// powerbi.visuals
-import DataViewCategoryColumn = powerbi.DataViewCategoryColumn;
-import Fill = powerbi.Fill;
-import ISandboxExtendedColorPalette = powerbi.extensibility.ISandboxExtendedColorPalette;
-import ISelectionId = powerbi.visuals.ISelectionId;
-import IVisual = powerbi.extensibility.IVisual;
-import IVisualHost = powerbi.extensibility.visual.IVisualHost;
-import PrimitiveValue = powerbi.PrimitiveValue;
-import SubSelectableDirectEdit = powerbi.visuals.SubSelectableDirectEdit;
-import SubSelectableDirectEditStyle = powerbi.visuals.SubSelectableDirectEditStyle;
 import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
-import DataViewObjectPropertyIdentifier = powerbi.DataViewObjectPropertyIdentifier;
 import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
-import ILocalizationManager = powerbi.extensibility.ILocalizationManager;
+import IVisual = powerbi.extensibility.visual.IVisual;
 
-
-/**
- * Interface for BarChart data points.
- *
- * @interface
- * @property {PrimitiveValue} value             - Data value for point.
- * @property {string} category          - Corresponding category of data value.
- * @property {string} color             - Color corresponding to data point.
- * @property {string} strokeColor       - Stroke color for data point column.
- * @property {number} strokeWidth       - Stroke width for data point column.
- * @property {ISelectionId} selectionId - Id assigned to data point for cross filtering
- *                                        and visual interaction.
- */
-export interface BarChartDataPoint {
-    value: PrimitiveValue;
-    category: string;
-    color: string;
-    strokeColor: string;
-    strokeWidth: number;
-    selectionId: ISelectionId;
-    index: number;
-    format?: string;
-}
-
-function createSelectorDataPoints(options: VisualUpdateOptions, host: IVisualHost): BarChartDataPoint[] {
-    const barChartDataPoints: BarChartDataPoint[] = []
-    const dataViews = options.dataViews;
-
-    if (!dataViews
-        || !dataViews[0]
-        || !dataViews[0].categorical
-        || !dataViews[0].categorical.categories
-        || !dataViews[0].categorical.categories[0].source
-        || !dataViews[0].categorical.values
-    ) {
-        return barChartDataPoints;
-    }
-
-    const categorical = dataViews[0].categorical;
-    const category = categorical.categories[0];
-    const dataValue = categorical.values[0];
-
-    //let dataMax: number = 0;
-
-    const colorPalette: ISandboxExtendedColorPalette = host.colorPalette;
-    //const objects = dataViews[0].metadata.objects;
-
-    const strokeColor: string = getColumnStrokeColor(colorPalette);
-
-    const strokeWidth: number = getColumnStrokeWidth(colorPalette.isHighContrast);
-
-    for (let i = 0, len = Math.max(category.values.length, dataValue.values.length); i < len; i++) {
-        const color: string = getColumnColorByIndex(category, i, colorPalette);
-
-        const selectionId: ISelectionId = host.createSelectionIdBuilder()
-            .withCategory(category, i)
-            .createSelectionId();
-
-        barChartDataPoints.push({
-            color,
-            strokeColor,
-            strokeWidth,
-            selectionId,
-            value: dataValue.values[i],
-            category: `${category.values[i]}`,
-            index: i,
-            format: dataValue.objects ? <string>dataValue.objects[i].general.formatString : null,
-        });
-    }
-
-    return barChartDataPoints;
-}
-
-function getColumnColorByIndex(
-    category: DataViewCategoryColumn,
-    index: number,
-    colorPalette: ISandboxExtendedColorPalette,
-): string {
-    if (colorPalette.isHighContrast) {
-        return colorPalette.background.value;
-    }
-
-    const defaultColor: Fill = {
-        solid: {
-            color: colorPalette.getColor(`${category.values[index]}`).value,
-        }
-    };
-
-    const prop: DataViewObjectPropertyIdentifier = {
-        objectName: "colorSelector",
-        propertyName: "fill"
-    };
-
-    let colorFromObjects: Fill;
-    if (category.objects?.[index]) {
-        colorFromObjects = dataViewObjects.getValue(category?.objects[index], prop);
-    }
-
-    return colorFromObjects?.solid.color ?? defaultColor.solid.color;
-}
-
-function getColumnStrokeColor(colorPalette: ISandboxExtendedColorPalette): string {
-    return colorPalette.isHighContrast
-        ? colorPalette.foreground.value
-        : null;
-}
-
-function getColumnStrokeWidth(isHighContrast: boolean): number {
-    return isHighContrast
-        ? 2
-        : 0;
-}
+import { VisualFormattingSettingsModel } from "./settings";
 
 export class Visual implements IVisual {
-    private barDataPoints: BarChartDataPoint[];
+    private target: HTMLElement;
+    private updateCount: number;
+    private textNode: Text;
+    private formattingSettings: VisualFormattingSettingsModel;
     private formattingSettingsService: FormattingSettingsService;
-    private formattingSettings: BarChartSettingsModel;
-    private host: IVisualHost;
-    private localizationManager: ILocalizationManager;
-    public visualOnObjectFormatting?: powerbi.extensibility.visual.VisualOnObjectFormatting;
 
     constructor(options: VisualConstructorOptions) {
-        this.host = options.host;
-        this.localizationManager = this.host.createLocalizationManager();
-        this.formattingSettingsService = new FormattingSettingsService(this.localizationManager);
+        console.log('Visual constructor', options);
+        this.formattingSettingsService = new FormattingSettingsService();
+        this.target = options.element;
+        this.updateCount = 0;
+        if (document) {
+            const new_p: HTMLElement = document.createElement("p");
+            new_p.appendChild(document.createTextNode("Update count:"));
+            const new_em: HTMLElement = document.createElement("em");
+            this.textNode = document.createTextNode(this.updateCount.toString());
+            new_em.appendChild(this.textNode);
+            new_p.appendChild(new_em);
+            this.target.appendChild(new_p);
+        }
     }
 
     public update(options: VisualUpdateOptions) {
-        this.formattingSettings = this.formattingSettingsService.populateFormattingSettingsModel(BarChartSettingsModel, options.dataViews?.[0]);
-        this.barDataPoints = createSelectorDataPoints(options, this.host);
-        this.formattingSettings.populateColorSelector(this.barDataPoints);
+        this.formattingSettings = this.formattingSettingsService.populateFormattingSettingsModel(VisualFormattingSettingsModel, options.dataViews[0]);
 
-        console.log(this.barDataPoints)
-
+        console.log('Visual update', options);
+        if (this.textNode) {
+            this.textNode.textContent = (this.updateCount++).toString();
+        }
     }
 
+    /**
+     * Returns properties pane formatting model content hierarchies, properties and latest formatting values, Then populate properties pane.
+     * This method is called once every time we open properties pane or when the user edit any format property. 
+     */
     public getFormattingModel(): powerbi.visuals.FormattingModel {
         return this.formattingSettingsService.buildFormattingModel(this.formattingSettings);
     }
-
-    public destroy(): void {
-        // Perform any cleanup tasks here
-    }
-
 }
