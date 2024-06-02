41bd9a0cdd08b193952ac313d572492465d3c925
PA <45755573+EquinetPaul@users.noreply.github.com>
2024-06-02 21:51:42 +0200

    display trajectories sorted by Z axis

--- src/settings.ts
+++ src/settings.ts
@@ -1,97 +1,3 @@
-<<<<<<< HEAD
-/*
- *  Power BI Visualizations
- *
- *  Copyright (c) Microsoft Corporation
- *  All rights reserved.
- *  MIT License
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the ""Software""), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in
- *  all copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- *  THE SOFTWARE.
- */
-
-"use strict";
-
-import { formattingSettings } from "powerbi-visuals-utils-formattingmodel";
-import powerbi from "powerbi-visuals-api";
-
-import FormattingSettingsCard = formattingSettings.SimpleCard;
-import FormattingSettingsSlice = formattingSettings.Slice;
-import FormattingSettingsModel = formattingSettings.Model;
-import IEnumMember = powerbi.IEnumMember
-
-import Slice = formattingSettings.Slice;
-import ItemDropdown = formattingSettings.ItemDropdown;
-import Card = formattingSettings.SimpleCard;
-
-/**
- * Data Point Formatting Card
- */
-class DataPointCardSettings extends FormattingSettingsCard {
-
-    public positionOptions: IEnumMember[] = [
-        { displayName: "Right", value: "Right" }, 
-        { displayName: "Left", value: "Left" }
-    ]
-
-    defaultColor = new formattingSettings.ColorPicker({
-        name: "defaultColor",
-        displayName: "Default color",
-        value: { value: "" },
-    });
-
-    fontSize = new formattingSettings.NumUpDown({
-        name: "fontSize",
-        displayName: "Text Size",
-        value: 12
-    });
-
-    enumMembers: IEnumMember[] = [
-        { value: "top", displayName: "Top" },
-        { value: "middle", displayName: "Middle" },
-        { value: "bottom", displayName: "Bottom" }
-    ];
-
-    position = new ItemDropdown({
-        name: "position",
-        displayName: "Position",
-        items: this.positionOptions,
-        value: this.positionOptions[0]
-    });
-
-    name: string = "dataPoint";
-    displayName: string = "Data colors";
-    slices: Array<FormattingSettingsSlice> = [this.defaultColor, this.fontSize, this.position];
-}
-
-/**
-* visual settings model class
-*
-*/
-export class VisualFormattingSettingsModel extends FormattingSettingsModel {
-    // Create formatting settings model formatting cards
-    dataPointCard = new DataPointCardSettings();
-
-
-    cards = [this.dataPointCard];
-
-}
-=======
 /*
  *  Power BI Visualizations
  *
@@ -125,16 +31,19 @@ import { formattingSettings } from "powerbi-visuals-utils-formattingmodel";
 import FormattingSettingsCard = formattingSettings.SimpleCard;
 import FormattingSettingsSlice = formattingSettings.Slice;
 import FormattingSettingsModel = formattingSettings.Model;
-import Slice = formattingSettings.Slice;
-import Card = formattingSettings.SimpleCard;
 import ColorPicker = formattingSettings.ColorPicker;
 
-import { DataPoint } from "./visual";
+import { dataPoint } from "./visual";
 
-class ColorSelectorCardSettings extends Card {
+import { dataViewObjectsParser } from 'powerbi-visuals-utils-dataviewutils';
+import DataViewObjectsParser = dataViewObjectsParser.DataViewObjectsParser;
+
+export class VisualSettings extends DataViewObjectsParser { }
+
+class ColorSelectorCardSettings extends FormattingSettingsCard {
     name: string = "colorSelector";
     displayName: string = "Data Colors";
-    slices: Slice[] = [];
+    slices: FormattingSettingsSlice[] = [];
 }
 
 /**
@@ -185,20 +94,6 @@ export class VisualFormattingSettingsModel extends FormattingSettingsModel {
     dataPointCard = new DataPointCardSettings();
     colorSelector = new ColorSelectorCardSettings();
 
-    cards = [this.colorSelector];
-
-    populateColorSelector(dataPoints: DataPoint[]) {
-        const slices: Slice[] = this.colorSelector.slices;
-        if (dataPoints) {
-            dataPoints.forEach(dataPoint => {
-                slices.push(new ColorPicker({
-                    name: "fill",
-                    displayName: dataPoint.category.toString(),
-                    value: { value: dataPoint.color },
-                    selector: dataPoint.selectionId.getSelector(),
-                }));
-            });
-        }
-    }
+    cards = [this.dataPointCard, this.colorSelector];
+    
 }
->>>>>>> cb0ceaaceca03c3d5d1b9bf91aa8b70c0656bf65
--- src/visual.ts
+++ src/visual.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
 *  Power BI Visual CLI
 *
@@ -28,124 +27,7 @@
 
 import powerbi from "powerbi-visuals-api";
 import { FormattingSettingsService } from "powerbi-visuals-utils-formattingmodel";
-import "./../style/visual.less";
-
-import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
-import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
-import IVisual = powerbi.extensibility.visual.IVisual;
-
-import { VisualFormattingSettingsModel } from "./settings";
-
-export class Visual implements IVisual {
-    private formattingSettings: VisualFormattingSettingsModel;
-    private formattingSettingsService: FormattingSettingsService;
-
-    constructor(options: VisualConstructorOptions) {
-
-        this.formattingSettingsService = new FormattingSettingsService();
- 
-    }
-
-    public update(options: VisualUpdateOptions) {
-        this.formattingSettings = this.formattingSettingsService.populateFormattingSettingsModel(VisualFormattingSettingsModel, options.dataViews?.[0]);
-
-        if (!options.dataViews || options.dataViews.length === 0 || !options.dataViews[0].table) {
-            return;
-        }
-    
-        let table = options.dataViews[0].table;
-        let rows = table.rows;
-        let xIndex = table.columns.findIndex(column => column.roles.x);
-        let yIndex = table.columns.findIndex(column => column.roles.y);
-        let zIndex = table.columns.findIndex(column => column.roles.z);
-        let legendIndex = table.columns.findIndex(column => column.roles.legend);
-
-        let legendValues = []
-        rows.forEach( row => {
-            legendValues.push(row[legendIndex])
-        })
-        const uniqueLegendValues = [...new Set(legendValues)];
-        console.log(uniqueLegendValues)
-        legendValues = undefined;
-
-        this.formattingSettings.dataPointCard.position.items.push(
-            { displayName: "test", value: "test" }
-        )
-
-        console.log(this.formattingSettings.dataPointCard.position.value)
-    
-        // let traceMap = new Map();
-    
-        // rows.forEach(row => {
-        //     let legend = row[legendIndex];
-        //     let trace = traceMap.get(legend);
-        //     if (!trace) {
-        //         trace = {
-        //             x: [],
-        //             y: [],
-        //             z: [],
-        //             type: 'scatter3d',
-        //             mode: 'markers',
-        //             name: legend,
-        //             marker: { size: 4 }
-        //         };
-        //         traceMap.set(legend, trace);
-        //     }
-        //     trace.x.push(row[xIndex]);
-        //     trace.y.push(row[yIndex]);
-        //     trace.z.push(row[zIndex]);
-        // });
-    
-        // let layout = {
-        //     title: '3D Scatter Plot',
-        //     scene: { 
-        //         xaxis: { title: 'X Axis' },
-        //         yaxis: { title: 'Y Axis' },
-        //         zaxis: { title: 'Depth' }
-        //     }
-        // };
-    
-        // Plotly.newPlot(this.targetElement, Array.from(traceMap.values()), layout);
-    }
-    
-
-    /**
-     * Returns properties pane formatting model content hierarchies, properties and latest formatting values, Then populate properties pane.
-     * This method is called once every time we open properties pane or when the user edit any format property. 
-     */
-    public getFormattingModel(): powerbi.visuals.FormattingModel {
-        return this.formattingSettingsService.buildFormattingModel(this.formattingSettings);
-    }
-=======
-/*
-*  Power BI Visual CLI
-*
-*  Copyright (c) Microsoft Corporation
-*  All rights reserved.
-*  MIT License
-*
-*  Permission is hereby granted, free of charge, to any person obtaining a copy
-*  of this software and associated documentation files (the ""Software""), to deal
-*  in the Software without restriction, including without limitation the rights
-*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-*  copies of the Software, and to permit persons to whom the Software is
-*  furnished to do so, subject to the following conditions:
-*
-*  The above copyright notice and this permission notice shall be included in
-*  all copies or substantial portions of the Software.
-*
-*  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-*  THE SOFTWARE.
-*/
-"use strict";
-
-import powerbi from "powerbi-visuals-api";
-import { FormattingSettingsService } from "powerbi-visuals-utils-formattingmodel";
+import { ColorHelper, hexToRGBString } from "powerbi-visuals-utils-colorutils";
 import "./../style/visual.less";
 
 import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
@@ -154,50 +36,161 @@ import IVisual = powerbi.extensibility.visual.IVisual;
 import ISelectionId = powerbi.visuals.ISelectionId;
 import IVisualHost = powerbi.extensibility.visual.IVisualHost;
 import DataViewTableRow = powerbi.DataViewTableRow;
-import ISandboxExtendedColorPalette = powerbi.extensibility.ISandboxExtendedColorPalette;
-import { dataViewObject, dataViewObjects } from "powerbi-visuals-utils-dataviewutils";
-
-import PrimitiveValue = powerbi.PrimitiveValue;
+import IColorPalette = powerbi.extensibility.IColorPalette;
+import ILocalizationManager = powerbi.extensibility.ILocalizationManager;
 import Fill = powerbi.Fill;
-import DataViewCategoryColumn = powerbi.DataViewCategoryColumn;
 import DataViewObjectPropertyIdentifier = powerbi.DataViewObjectPropertyIdentifier;
-import DataViewObjects = powerbi.DataViewObjects;
-import DataViewObject = powerbi.DataViewObject;
+import { dataViewObjects } from "powerbi-visuals-utils-dataviewutils";
+import Plotly from 'plotly.js-dist';
+import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
+import VisualDataChangeOperationKind = powerbi.VisualDataChangeOperationKind;
 
 import { VisualFormattingSettingsModel } from "./settings";
+import { VisualSettings } from "./settings";
+import { color, group } from "d3";
 
-export interface DataPoint {
-    value_x: PrimitiveValue;
-    value_y: PrimitiveValue;
-    category: PrimitiveValue;
-    color: string;
-    selectionId: ISelectionId;
-    index: number;
+export interface dataPoint {
+    x: powerbi.PrimitiveValue,
+    y: powerbi.PrimitiveValue,
+    z: powerbi.PrimitiveValue,
+    legend: powerbi.PrimitiveValue,
+    group: powerbi.PrimitiveValue,
+    selection: ISelectionId
 }
 
 export class Visual implements IVisual {
-
     private formattingSettings: VisualFormattingSettingsModel;
     private formattingSettingsService: FormattingSettingsService;
     private host: IVisualHost;
-    private DataPoints: DataPoint[];
+    private dataPoints: dataPoint[] = []
+    private dataPointsUniqueLegend: dataPoint[]
+    private localizationManager: ILocalizationManager;
+    private colorPalette: IColorPalette;
+
+    private target: HTMLElement;
+    private updateCount: number;
+    private settings: VisualSettings;
+    private textNode: Text;
+    private windowsLoaded: number;
+
 
     constructor(options: VisualConstructorOptions) {
-
-        this.formattingSettingsService = new FormattingSettingsService();
-
+        this.formattingSettingsService = new FormattingSettingsService(this.localizationManager);
         this.host = options.host;
+        this.colorPalette = options.host.colorPalette;
+
+        console.log('Visual constructor', options);
+        this.target = options.element;
+        this.updateCount = 0;
+        this.windowsLoaded = 0;
+        this.host = options.host;
+        if (typeof document !== 'undefined') {
+            const new_p: HTMLElement = document.createElement('p');
+            new_p.appendChild(document.createTextNode('Message:'));
+            const new_em: HTMLElement = document.createElement('em');
+            this.textNode = document.createTextNode(this.updateCount.toString());
+            new_em.appendChild(this.textNode);
+            new_p.appendChild(new_em);
+            this.target.appendChild(new_p);
+        }
     }
 
     public update(options: VisualUpdateOptions) {
-        // const DataPoints: DataPoint[] = []
+        this.formattingSettings = this.formattingSettingsService.populateFormattingSettingsModel(VisualFormattingSettingsModel, options.dataViews[0]);
 
-        this.formattingSettings = this.formattingSettingsService.populateFormattingSettingsModel(VisualFormattingSettingsModel, options.dataViews?.[0]);
-        this.DataPoints = createSelectorDataPoints(options, this.host);
-        this.formattingSettings.populateColorSelector(this.DataPoints)
+        if (!options.dataViews || options.dataViews.length === 0 || !options.dataViews[0].table) {
+            return;
+        }
 
-        console.log("ok")
+        // Fetch More Data
+        this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
 
+        console.log('Visual update', options, this.settings);
+        console.log('Update kind', options.operationKind);
+        if (options.operationKind === VisualDataChangeOperationKind.Create) {
+            this.windowsLoaded = 1;
+        }
+        if (options.operationKind === VisualDataChangeOperationKind.Append) {
+            this.windowsLoaded += 1;
+        }
+
+        let rowCount = options.dataViews[0].table.rows.length;
+
+        if (options.dataViews[0].metadata.segment) {
+            this.textNode.textContent = `Loading more data. ${rowCount} rows loaded so far (over ${this.windowsLoaded} fetches)...`;
+            let canFetchMore = this.host.fetchMoreData();
+            if (!canFetchMore) {
+                this.textNode.textContent = `Memory limit hit after ${this.windowsLoaded} fetches. We managed to get ${rowCount} rows.`;
+            }
+        } else {
+            this.textNode.textContent = `We have all the data we can get (${rowCount} rows over ${this.windowsLoaded} fetches)!`;
+        }
+
+        const table = options.dataViews[0].table;
+
+        // Transform data
+        this.dataPoints = this.transformTable(table)
+
+        // Draw visual
+        var gd = document.querySelector('div');
+        const legendTraces = {};
+
+        // Parcourir les données pour les organiser par légende
+        this.dataPoints.forEach(point => {
+            const group = point.group;
+            const legend = point.legend as string;
+
+            if (!legendTraces[legend]) {
+                legendTraces[legend] = [];
+            }
+
+            legendTraces[legend].push(point);
+        });
+
+        // Trier les points par valeur de Z pour chaque légende
+        Object.keys(legendTraces).forEach(legend => {
+            legendTraces[legend].sort((a, b) => a.z - b.z);
+        });
+
+        // Préparer les données pour Plotly après avoir trié
+        const traces = Object.keys(legendTraces).map(legend => {
+            const trace = {
+                x: [],
+                y: [],
+                z: [],
+                mode: 'lines',
+                type: 'scatter3d',
+                name: legend,
+                legendgroup: legendTraces[legend][0].group,
+                text: [],
+                marker: { size: 3 },
+                line: {
+                    color: hexToRGBString(this.colorPalette.getColor(legendTraces[legend][0].legend).value)
+                }
+            };
+
+            legendTraces[legend].forEach(point => {
+                trace.x.push(point.x);
+                trace.y.push(point.y);
+                trace.z.push(point.z);
+                trace.text.push(point.legend);
+            });
+
+            return trace;
+        });
+
+        // Définir la mise en page du graphique
+        const layout = {
+            // title: '3D Scatter Plot',
+            scene: {
+                // xaxis: { title: 'X Axis' },
+                // yaxis: { title: 'Y Axis' },
+                // zaxis: { title: 'Z Axis' }
+            },
+            showlegend: true
+        };
+
+        Plotly.newPlot(gd, traces, layout);
     }
 
     /**
@@ -207,99 +200,41 @@ export class Visual implements IVisual {
     public getFormattingModel(): powerbi.visuals.FormattingModel {
         return this.formattingSettingsService.buildFormattingModel(this.formattingSettings);
     }
-}
 
-function createSelectorDataPoints(options: VisualUpdateOptions, host: IVisualHost): DataPoint[] {
-    const DataPoints: DataPoint[] = []
-    const dataViews = options.dataViews;
+    public transformTable(table: powerbi.DataViewTable): dataPoint[] {
+        const dataPoints: dataPoint[] = []
 
-    if (!dataViews
-        || !dataViews[0]
-        || !dataViews[0].table
-    ) {
-        return DataPoints;
-    }
+        const xIndex = table.columns.findIndex(column => column.roles.x);
+        const yIndex = table.columns.findIndex(column => column.roles.y);
+        const zIndex = table.columns.findIndex(column => column.roles.z);
+        const legendIndex = table.columns.findIndex(column => column.roles.legend);
+        const groupIndex = table.columns.findIndex(column => column.roles.group);
 
+        table.rows.forEach((row, rowIndex) => {
+            const xValue = row[xIndex];
+            const yValue = row[yIndex];
+            const zValue = row[zIndex];
+            const legendValue = row[legendIndex];
+            const groupValue = row[groupIndex];
+            const selection: ISelectionId = this.host.createSelectionIdBuilder()
+                .withTable(table, rowIndex)
+                .createSelectionId();
 
-
-    const dataView = options.dataViews[0];
-    const colorPalette: ISandboxExtendedColorPalette = host.colorPalette;
-
-    let table = options.dataViews[0].table;
-    let rows = table.rows;
-    let xIndex = table.columns.findIndex(column => column.roles.measure);
-    let yIndex = table.columns.findIndex(column => column.roles.measure2);
-    let legendIndex = table.columns.findIndex(column => column.roles.category);
-
-    let categories: DataViewCategoryColumn = {
-        values: [],
-        source: dataView.metadata.columns[legendIndex]
-    }
-    rows.forEach(row => {
-        categories.values.push(row[legendIndex])
-    })
-
-    dataView.table.rows.forEach((row: DataViewTableRow, rowIndex: number) => {
-        const x = row[xIndex]
-        const y = row[yIndex]
-        const legend = row[legendIndex]
-        const color = colorPalette.getColor(legend.toString()).value
-        // const color = getColumnColorByIndex(categories, rowIndex, colorPalette)
-
-        const selection: ISelectionId = host.createSelectionIdBuilder()
-            .withTable(dataView.table, rowIndex)
-            .createSelectionId();
-
-
-        DataPoints.push({
-            value_x: x,
-            value_y: y,
-            category: legend,
-            color: color,
-            selectionId: selection,
-            index: rowIndex
+            dataPoints.push({
+                x: xValue,
+                y: yValue,
+                z: zValue,
+                legend: legendValue,
+                group: groupValue,
+                selection: selection
+            })
         })
 
-    })
-
-    return DataPoints;
-
-}
-
-// TO DO
-// HOW TO ADAPT THIS METHOD TO GET THE COLOR EITHER FROM:
-//     PALETTE (DEFAULT BEHAVIOR)
-//     OBJECT  (WHEN DEFINED BY THE USER IN PROPERTY PANE)
-
-// dataViewObjects.getValue(category:powerbi.DataViewCategoryColumn)
-// Can't find a solution to adapt this method to a data role/mapping table
-function getColumnColorByIndex(
-    category: DataViewCategoryColumn,
-    index: number,
-    colorPalette: ISandboxExtendedColorPalette,
-): string {
-    if (colorPalette.isHighContrast) {
-        return colorPalette.background.value;
+        return dataPoints;
     }
 
-    const defaultColor: Fill = {
-        solid: {
-            color: colorPalette.getColor(`${category.values[index]}`).value,
-        }
-    };
-
-    const prop: DataViewObjectPropertyIdentifier = {
-        objectName: "colorSelector",
-        propertyName: "fill"
-    };
-
-    let colorFromObjects: Fill;
-    if (category.objects?.[index]) {
-        colorFromObjects = dataViewObjects.getValue(category?.objects[index], prop);
+    private static parseSettings(dataView: powerbi.DataView): VisualSettings {
+        return VisualSettings.parse(dataView) as VisualSettings;
     }
 
-    let result = colorFromObjects?.solid.color ?? defaultColor.solid.color;
-
-    return result
->>>>>>> cb0ceaaceca03c3d5d1b9bf91aa8b70c0656bf65
 }
